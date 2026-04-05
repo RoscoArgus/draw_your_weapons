cc644f1e04a2ad3bcd8cc1c152fd29f0adb9f2d6
James <jdoyle7103@gmail.com>
2026-04-05 20:06:33 +0100

    made enemies not weird coloured

--- Unity/Assets/Scripts/Combat/EnemyVisuals.cs
+++ Unity/Assets/Scripts/Combat/EnemyVisuals.cs
@@ -21,28 +21,5 @@ public class EnemyVisuals : MonoBehaviour
 
     private void ApplyColor()
     {
-        if (health == null) return;
-
-        Color color = Color.white;
-
-        switch (health.enemyType)
-        {
-            case EnemyType.WeakToSlashing:
-                color = Color.red;
-                break;
-            case EnemyType.WeakToPiercing:
-                color = Color.blue;
-                break;
-            case EnemyType.WeakToBlunt:
-                color = Color.yellow;
-                break;
-        }
-
-        foreach (Renderer r in targetRenderers)
-        {
-            if (r == null) continue;
-            foreach (Material mat in r.materials)
-                mat.color = color;
-        }
     }
 }
